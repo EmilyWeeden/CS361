c757d2f096604eca8821cafe6cb090b22925d1da
Emily Weeden <zzz959@vols.utk.edu>
2018-09-17 21:37:12 -0400

    wrote outline

--- Lab1/snprintf.c
+++ Lab1/snprintf.c
@@ -6,6 +6,9 @@
 int snprintf(char *dst, size_t buffer_size, const char *format, ...);
 
 //Other function calls
+static void convertint(char *str, int val){
+    //do something
+}
 
 //Main for testing
 int main() {
@@ -33,18 +36,72 @@ int snprintf(char *dst, size_t buffer_size, const char *format, ...){
         if (*format == '%'){
             format++;
             //Check for flag
+            switch (*format){
+                case '-': { //left justified
+                    break;
+                }
+                case '+': { //changes sign
+                    break;
+                }
+                case '#': { //print "0x"
+                    break;
+                }
+                case '0': { //fill with 0s instead of spaces
+                    break;
+                }
+            }
             //Check for specifier
             switch (*format){
-                case 's': {
+                case 's': { //string
                     const char *s = va_arg(arg, const char *);
                     while (*s)
                         putchar(*s++);
                     break;
                 }
-                case 'd': {
+                case 'c': { //character
+                    const char *c = va_arg(arg, const char **);
+                    putchar(*s++);
+                    break;
+                }
+                case 'x': { //32bit hex number
+                    int x = va_arg(arg, int);
+                    break;
+                }
+                case 'lx': { //64bit hex number
+                    int lx = va_arg(arg, int);
+                    break;
+                }
+                case 'd': { //32bit signed decimal
                     int d = va_arg(arg, int);
                     //do something
                     break;
+                }
+                case 'ld': { //64bit signed decimal
+                    int ld = va_arg(arg, int);
+                    break;
+                }
+                case 'u': { //32bit unsigned decimal
+                    unsigned int u = va_arg(arg, unsigned int);
+                    break;
+                }
+                case 'lu': { //64bit unsigned decimal
+                    unsigned int lu = va_arg(arg, unsigned int);
+                    break;
+                }
+                case 'p': { //64bit pointer in hex
+                    //pointer
+                    break;
+                }
+                case 'f': { //32bit float number
+                    float f = va_arg(arg, float);
+                    break;
+                }
+                case 'lf': { //64bit float number
+                    float lf = va_arg(arg, float);
+                    break;
+                }
+                case '%': { //just %
+                    //%
                 }
             }
             format++;
